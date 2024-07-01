dadd02fe911fc6a15beea81055f50cb1042535ff
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-07-01 15:12:21 +0200

    CepGen: added test for a simple event generation; Generator: added a Common interface to handle HepMC3 event content translation

--- Generator/CepGenInterface/src/CepGenEventGenerator.cc
+++ Generator/CepGenInterface/src/CepGenEventGenerator.cc
@@ -16,28 +16,70 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <CepGen/Core/Exception.h>
+#include <CepGen/Core/ParametersList.h>
+#include <CepGen/Core/RunParameters.h>
+#include <CepGen/EventFilter/EventExporter.h>
+#include <CepGen/EventFilter/EventModifier.h>
+#include <CepGen/Modules/EventExporterFactory.h>
+#include <CepGen/Modules/EventModifierFactory.h>
+#include <CepGen/Modules/ProcessFactory.h>
+#include <CepGen/Process/Process.h>
 #include <CepGenAddOns/HepMC3Wrapper/HepMC3EventInterface.h>
 #include <DDG4/Factories.h>
 #include <DDG4/Geant4InputAction.h>
 
 #include "Generator/CepGenInterface/include/CepGenEventGenerator.h"
+#include "Generator/Common/include/HepMC3EventConverter.h"
 
 using EventReaderStatus = dd4hep::sim::Geant4EventReader::EventReaderStatus;
 
 CepGenEventGenerator::CepGenEventGenerator(const std::string& filename)
-    : dd4hep::sim::Geant4EventReader(filename), cepgen_(new cepgen::Generator) {}
+    : dd4hep::sim::Geant4EventReader(filename), cepgen_(new cepgen::Generator), xsec_(new HepMC3::GenCrossSection) {}
 
 EventReaderStatus CepGenEventGenerator::moveToEvent(int) { return EVENT_READER_OK; }
 
 EventReaderStatus CepGenEventGenerator::readParticles(int, Vertices& vertices, Particles& particles) {
-  vertices.clear();
-  particles.clear();
-  return EVENT_READER_OK;
+  auto* evt = new HepMC3::CepGenEvent(cepgen_->next());
+  evt->set_cross_section(xsec_);
+  evt->weights().push_back(1.);
+  if (HepMC3EventConverter::convert(evt, vertices, particles))
+    return EVENT_READER_OK;
+  return EVENT_READER_IO_ERROR;
 }
 
 EventReaderStatus CepGenEventGenerator::setParameters(std::map<std::string, std::string>& parameters) {
-  std::list<std::string> commands;
-  _getParameterValue(parameters, "Commands", commands, commands);
+  int verb = 0;
+  std::list<std::string> process_commands;
+  std::vector<std::list<std::string> > modif_commands, output_commands;
+  _getParameterValue(parameters, "Verbosity", verb, verb);
+  _getParameterValue(parameters, "Process", process_commands, process_commands);
+  _getParameterValue(parameters, "Modifier", modif_commands, modif_commands);
+  _getParameterValue(parameters, "Output", output_commands, output_commands);
+
+  cepgen::utils::Logger::get().setLevel(static_cast<cepgen::utils::Logger::Level>(verb));
+
+  cepgen::ParametersList plist_proc;
+  for (const auto& cmd : process_commands)
+    plist_proc.feed(cmd);
+  cepgen_->runParameters().setProcess(cepgen::ProcessFactory::get().build(plist_proc));
+
+  for (const auto& modif : modif_commands) {
+    cepgen::ParametersList plist_mod;
+    for (const auto& cmd : modif)
+      plist_mod.feed(cmd);
+    cepgen_->runParameters().addModifier(cepgen::EventModifierFactory::get().build(plist_mod));
+  }
+
+  for (const auto& output : output_commands) {
+    cepgen::ParametersList plist_mod;
+    for (const auto& cmd : output)
+      plist_mod.feed(cmd);
+    cepgen_->runParameters().addEventExporter(cepgen::EventExporterFactory::get().build(plist_mod));
+  }
+
+  const auto xsec = cepgen_->computeXsection();
+  xsec_->set_cross_section(xsec, xsec.uncertainty());
 
   return EVENT_READER_OK;
 }
---
+++ test/LHeD_CepGen_event.py
@@ -0,0 +1,75 @@
+"""
+
+   Run simulation using Geant4/BDSIM input file loader
+
+   @author  L.Forthomme
+   @version 1.0
+
+"""
+from __future__ import absolute_import, unicode_literals
+import logging
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def run():
+    import os
+    import DDG4
+    from DDG4 import OutputLevel as Output
+    import LHeD
+
+    lhed = LHeD.LHeD()
+    geant4 = lhed.geant4
+    kernel = lhed.kernel
+
+    lhed.loadGeometry()
+    geant4.printDetectors()
+    # Configure UI
+    #geant4.setupCshUI(vis=True)
+    #geant4.setupCshUI()
+    #geant4.setupUI(typ='qt', vis=True)
+    geant4.setupUI(vis=False)
+
+    prt = DDG4.EventAction(kernel, 'Geant4ParticlePrint/ParticlePrint')
+    prt.OutputLevel = Output.WARNING
+    prt.OutputType = 3  # Print both: table and tree
+    kernel.eventAction().adopt(prt)
+
+    geant4.setupROOTOutput('RootOutput', 'output.root')
+
+    gen = DDG4.GeneratorAction(kernel, "Geant4InputAction/Input")
+    gen.Input = 'CepGenEventGenerator'
+    gen.OutputLevel = Output.DEBUG
+    gen.Parameters = dict(
+        Process = [
+            'name:lpair',
+            'mode:1',
+        ],
+    )
+    geant4.buildInputStage([gen], output_level=Output.DEBUG)
+
+    part = DDG4.GeneratorAction(kernel, 'Geant4ParticleHandler/ParticleHandler')
+    kernel.generatorAction().adopt(part)
+    part.enableUI()
+
+    lhed.setupDetectors()
+    # Now build the physics list:
+    lhed.setupPhysics('QGSP_BERT')
+    lhed.test_config()
+
+    #scan = DDG4.SteppingAction(kernel, 'Geant4MaterialScanner/MaterialScan')
+    #kernel.steppingAction().adopt(scan)
+
+    #geant4.run()
+
+    kernel.NumEvents = 10
+
+    kernel.run()
+    kernel.terminate()
+    logger.info('End of run. Terminating .......')
+    logger.info('TEST_PASSED')
+
+
+if __name__ == "__main__":
+  run()
