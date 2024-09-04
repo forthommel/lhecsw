847c79d6e3cdfd3d9fb62c36011149069ca26d4e
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-09-04 14:41:32 +0200

    Generator: simplification of CepGen steering through intermediate python parameter object (#5)
    * Generator: using an intermediate JSON object to parameterise the CepGen run
    
    * CepGen: further simplification of event modifier/output module steering with CepGenParameters python object

--- python/Generator/cepgenInterface_cff.py
+++ python/Generator/cepgenInterface_cff.py
@@ -3,8 +3,26 @@ from Configurables import CepGenEventGenerator
 from Configurables import HepMCToEDMConverter, SimG4PrimariesFromEdmTool
 
 
+class CepGenParameters(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+        super(CepGenParameters, self).__init__(*args, **kwargs)
+
+    def __str__(self):
+        from json import dumps
+        return dumps(self)
+
+class CepGenModule(CepGenParameters):
+    def __init__(self, name, *args, **kwargs):
+        super(CepGenModule, self).__init__(*args, **kwargs)
+        self.mod_name = name
+
 cepgen = CepGenEventGenerator("CepGenEventGenerator",
-    process = [],
+    process = '',
     OutputLevel = INFO,
 )
 
